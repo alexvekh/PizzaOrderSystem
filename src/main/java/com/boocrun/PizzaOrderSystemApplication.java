3ba648904f4e6d5f8c6dd257e98788337cc24aff
Oleksiy Vekhulevskyy <alexvekh@yahoo.com>
2022-12-03 14:37:16 -0500

    Start

---
+++ src/main/java/com/boocrun/PizzaOrderSystemApplication.java
@@ -0,0 +1,13 @@
+package com.boocrun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzaOrderSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzaOrderSystemApplication.class, args);
+	}
+
+}
