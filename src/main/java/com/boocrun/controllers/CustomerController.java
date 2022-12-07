055628aa1084394527a4da1b06691364712b4fe5
Oleksiy Vekhulevskyy <alexvekh@yahoo.com>
2022-12-06 23:47:53 -0500

    Created CustomerController and template

---
+++ src/main/java/com/boocrun/controllers/CustomerController.java
@@ -0,0 +1,37 @@
+package com.boocrun.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.boocrun.domain.Customer;
+import com.boocrun.repositories.CustomerRepository;
+
+@Controller
+public class CustomerController {
+  
+  @Autowired
+  private CustomerRepository customerRepo;
+  
+  @RequestMapping(value="/", method=RequestMethod.GET)
+  public String rootRedirect () {
+    return "redirect:/customers";
+  }
+  
+  @RequestMapping(value="/customers", method=RequestMethod.GET)
+  public String customers (ModelMap model) {
+    Customer customer = new Customer();
+    model.put("customer", customer);
+    return "customers";
+  }
+  
+  @RequestMapping(value="/customers", method=RequestMethod.POST)
+  public String customerPost (@ModelAttribute Customer customer, ModelMap model) {
+    customerRepo.save(customer);
+    return "redirect:/orders";
+  }
+}
+
